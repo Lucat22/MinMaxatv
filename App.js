38b9b09e4f8dab5b651ade58695345da4e3e942a
lucat22 <lucalvins1@gmail.com>
2023-08-24 21:12:13 -0300

    Incluindo Numero Aleatorio

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import MinMax from './components/MinMax';
+import MinMax from './components/exercicio1/MinMax';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import NumeroAleatorio from './components/Exercicio2/NumeroAleatorio';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <MinMax min='20' max='50'/>
+      <NumeroAleatorio min={1} max={100}/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
---
+++ components/Exercicio2/NumeroAleatorio.js
@@ -0,0 +1,11 @@
+import react from "react";
+import { Text } from "react-native";
+
+const NumeroAleatorio = ({ min, max }) => {
+    const numero =
+    Math.floor(Math.random() * (max - min + 1)) + min;
+
+    return <Text>O número aleatório é: {numero}</Text>;
+};
+
+export default NumeroAleatorio;
