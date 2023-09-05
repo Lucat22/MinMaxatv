f0d14510a46bcb8e29cdaff57181cbb6da9711a9
lucat22 <lucalvins1@gmail.com>
2023-09-05 13:32:18 -0300

    Aplicativo 'BiscoitodaSorte'

--- App.js
+++ App.js
@@ -1,22 +1,11 @@
-import React from 'react';
-import MinMax from './components/exercicio1/MinMax';
-import { StatusBar } from 'expo-status-bar';
+import { statusbar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import NumeroAleatorio from './components/Exercicio2/NumeroAleatorio';
-import Botao from './components/exercicio3/Botao';
-import Contador from './components/exercicio3/Contador';
-import DigiteSeuNome from './components/exercicio3/DigiteSeuNome';
+import BiscoitodaSorte from './components/BiscoitodaSorte'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <DigiteSeuNome></DigiteSeuNome>
-      <Botao></Botao>
-      <Contador></Contador>
-      <MinMax min='20' max='50'/>
-      <NumeroAleatorio min={1} max={100}/>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <BiscoitodaSorte />
     </View>
   );
 }
@@ -26,6 +15,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
